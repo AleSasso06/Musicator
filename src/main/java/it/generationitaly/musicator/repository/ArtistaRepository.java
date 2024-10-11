20537dabf2fe51f4031fe5a3b9248d17f7f903e8
Rosa Piscitelli <piscitelli.rosa98@gmail.com>
2024-10-11 11:52:16 +0200

    metodi aggiunti: -findByNome -findByNazionalita

--- src/main/java/it/generationitaly/musicator/repository/ArtistaRepository.java
+++ src/main/java/it/generationitaly/musicator/repository/ArtistaRepository.java
@@ -1,7 +1,13 @@
 package it.generationitaly.musicator.repository;
 
+import java.util.List;
+
 import it.generationitaly.musicator.entity.Artista;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long>{
 
+	Artista findByNome(String pseudonimo);
+	List<Artista> findByNazionalita(String nazionalita);
+	
+	
 }
