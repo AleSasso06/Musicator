b866bcbc2a80788fe9296dcb1a20fe92b9a356ef
alice <alice@192.168.1.7>
2024-10-16 12:20:23 +0200

    risolto problema registrazione

--- src/main/java/it/generationitaly/musicator/controller/RegistratiServlet.java
+++ src/main/java/it/generationitaly/musicator/controller/RegistratiServlet.java
@@ -16,13 +16,12 @@ import it.generationitaly.musicator.repository.impl.UtenteRepositoryImpl;
 
 public class RegistratiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private UtenteRepository utenteRepository = new UtenteRepositoryImpl();
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Utente utente = new Utente();
-		
+
 		String foto = "https://img.freepik.com/vettori-premium/icona-del-profilo-utente-in-stile-piatto-illustrazione-del-vettore-avatar-membro-su-sfondo-isolato-concetto-di-business-del-segno-di-autorizzazione-umana_157943-15752.jpg";
 		String nome = request.getParameter("nome");
 		String cognome = request.getParameter("cognome");
@@ -30,49 +29,32 @@ public class RegistratiServlet extends HttpServlet {
 		// String dataNascitaAsString = request.getParameter("dataNascita");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
-		/*Date dataNascita = null;
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		try {
-			dataNascita = sdf.parse(dataNascitaAsString);
-		} catch (ParseException e) {
-			System.err.println("Errore, formato non corretto");
-			return;
-		}*/
-		
-		System.out.println(username);
-		
-		if (utente.getUsername().equalsIgnoreCase(username)) {
-			response.sendRedirect("registrati.jsp?erroreUsernameEsistente");
-			return;
-		}
-		
-		System.out.println("Foto: " + foto);
-		System.out.println("Nome: " + nome);
-		System.out.println("Cognome: " + cognome);
-		System.out.println("Email: " + email);
-		// System.out.println("Data di nascita: " + dataNascita);
-		System.out.println("Username: " + username);
-		System.out.println("Password: " + password);
-		
-		utente = utenteRepository.findByUsername(username);
-		if (utente == null) {
-			utente = new Utente();
-			utente.setFoto(foto);
-			utente.setNome(nome);
-			utente.setCognome(cognome);
-			utente.setEmail(email);
-			// utente.setDataNascita(dataNascita);
-			utente.setUsername(username);
-			utente.setPassword(password);
 
-			utenteRepository.save(utente);
-			response.sendRedirect("login.jsp");
-		} else {
+		/*
+		 * Date dataNascita = null; SimpleDateFormat sdf = new
+		 * SimpleDateFormat("dd/MM/yyyy"); try { dataNascita =
+		 * sdf.parse(dataNascitaAsString); } catch (ParseException e) {
+		 * System.err.println("Errore, formato non corretto"); return; }
+		 */
+
+		System.out.println(username);
+
+		if (utenteRepository.findByUsername(username) != null) {
 			response.sendRedirect("registrati.jsp?erroreUsernameEsistente");
+			return;
 		}
+
+		Utente utente = new Utente();
+		utente.setFoto(foto);
+		utente.setNome(nome);
+		utente.setCognome(cognome);
+		utente.setEmail(email);
+		// utente.setDataNascita(dataNascita);
+		utente.setUsername(username);
+		utente.setPassword(password);
+
+		utenteRepository.save(utente);
+		response.sendRedirect("login.jsp");
 	}
 
-	
-	
 }
