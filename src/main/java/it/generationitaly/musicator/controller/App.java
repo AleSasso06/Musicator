e8f834800303cd77404b410cecac34ff4b09970b
Carlos <Carlos@Carlos-PC.station>
2024-10-19 14:34:34 +0200

    modifiche a detatglio playlist

--- src/main/java/it/generationitaly/musicator/controller/App.java
+++ src/main/java/it/generationitaly/musicator/controller/App.java
@@ -18,20 +18,27 @@ public class App {
 
 	private static BranoRepository branoRepository = new BranoRepositoryImpl();
 	private static AlbumRepository albumRepository = new AlbumRepositoryImpl();
-
+	private static PlaylistRepository playlistRepository = new PlaylistRepositoryImpl();
 	public static void main(String[] args) {
 	
-		Album album = albumRepository.findById(Long.parseLong("6"));
-		
-		List<Brano> brani = branoRepository.findByAlbumId(6);
-		
-		if(brani != null && !brani.isEmpty()) {
-			for (Brano brano : brani) {
-				System.out.println(brano.getTitolo());
-			}
-		} else {
-			System.out.println("Nessun brano trovato");
+		Playlist playlist = playlistRepository.findById(Long.parseLong("1"));
+		List<Brano> brani = playlist.getBrani();
+		for (Brano brano : brani) {
+			System.out.println(brano);
 		}
-	
+		
+		
+//		Album album = albumRepository.findById(Long.parseLong("6"));
+//		
+//		List<Brano> brani = branoRepository.findByAlbumId(6);
+//		
+//		if(brani != null && !brani.isEmpty()) {
+//			for (Brano brano : brani) {
+//				System.out.println(brano.getTitolo());
+//			}
+//		} else {
+//			System.out.println("Nessun brano trovato");
+//		}
+//	
 	}
 }
